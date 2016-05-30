8b2d7f2144ffde2e890ab44352fcd140978ab08e
Montasser Mossallem <moun2030@gmail.com>
2016-05-30 23:55:03 +0300

    fix header already sent

--- privates/private_access.php
+++ privates/private_access.php
@@ -3,8 +3,7 @@
   * This acess for define who is paid to course , to secure all courses without paid 
   */
 @ob_start();
- if(session_id() =='')
-     session_start () ;
+ 
  // check about public access 
      $public_file = dirname(__FILE__)."/../modular/apis/website_setting_apis.php";
      if(is_file($public_file)) require_once $public_file ;
@@ -20,6 +19,6 @@
              exit();
          }
      }
- session_write_close() ;
+ 
  ob_end_flush();
 ?>
