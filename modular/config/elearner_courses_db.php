e99bd91eded56bd0097bba81e1a83bae8220d823
Montasser Mossallem <moun2030@gmail.com>
2016-05-30 23:42:40 +0300

    fix database connections

--- modular/config/elearner_courses_db.php
+++ modular/config/elearner_courses_db.php
@@ -10,9 +10,9 @@
 class database {
     private $db_connection =  NULL ;
     private static $HOST_NAME = "localhost";
-    private static $DB_NAME = "mon2000_courses";
-    private static $DB_USER_NAME = "mon2000_mon2000";
-    private static $DB_PASSWORD= "bobsushi666666";
+    private static $DB_NAME = "mon2000_sweegy";
+    private static $DB_USER_NAME = "mon2000_sweegy";
+    private static $DB_PASSWORD= "666666";
     
     
 
